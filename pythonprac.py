93b307506d070115ff1456795c05c8781896c98e
auberr <auberr@daum.net>
2022-10-13 11:03:24 +0900

    init project

---
+++ pythonprac.py
@@ -0,0 +1,14 @@
+class SomeClassName : # Pascal convention
+    pass
+
+class someClassName : # Camel convention
+    pass
+
+class some_class_name : # snake convention
+    pass
+
+tuple = (1,2,3,4,5) # 선언 후 변경 삭제 불가
+
+sets = {1, 2, 3, 4, 5} # 중복된 데이터 없음
+
+dict = {'number': '1'}
