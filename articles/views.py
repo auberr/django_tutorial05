4e10434e1b7a8bdf322aa3bb61a32bb131df9f38
auberr <auberr@daum.net>
2022-10-13 11:52:08 +0900

    Add dinner feature

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menus)
+    context = {
+        'pick': pick,
+        'name': name,
+    }
+    return render(request, 'dinner.html', context)
